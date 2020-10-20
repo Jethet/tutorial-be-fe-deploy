9488a21137231549d1d0e406488a8c2d92624126
Jethet <henriette.hettinga@gmail.com>
2020-10-20 17:12:22 +0200

    Create App, add Bootstrap to index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* Initialize all the states inside the constructor: STATES TO STORE DATA are
+1) list: if the state list is true, the list of users component will be displayed.
+2) card: if the state card is true, the single-user card will be displayed.
+3) users: this state stores the list array of players from the backend.
+4) user: this stores a single-user detail from the backend.
+*/
+
+export default class App extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      list: true,
+      card: false,
+      users: [],
+      user: {}
+    }
+  }
 }
-
-export default App;
