e481908c9555ab6d618c598f9f35348576003218
Jethet <henriette.hettinga@gmail.com>
2020-11-03 14:44:37 +0100

    Start fcc react and node/express tutorial

--- backend/dummyDB.js
+++
@@ -1,7 +0,0 @@
-let users = [
-  { _id: 1, name: "Anna", points: "18426" },
-  { _id: 2, name: "Helen", points: "10500" },
-  { _id: 3, name: "Suzan", points: "10843" },
-];
-
-module.exports = users;
